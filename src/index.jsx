e45b5d4b6cb5330db2b3b5258378405fe3f4b6be
thithuha nguyen <chloe.nguyen1910@gmail.com>
2021-10-05 14:54:10 +0200

    create App component

--- src/index.jsx
+++ src/index.jsx
@@ -2,45 +2,13 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
+import App from './components/app';
 
-// const Hello = ({ name }) => {
-//   return (
-//     <div>
-//       Hello,
-//       {name}
-//     </div>
-//   );
-// };
-class Hello extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      clicked: true,
-      counter: 0
-    };
-  }
-
-  handleClick = () => {
-    // TODO: change the state!
-    this.setState({
-      clicked: !this.state.clicked,
-      counter: this.state.counter + 1
-    });
-  }
-
-  render() {
-    // Build and return HTML
-    return (
-      <div className={this.state.clicked ? 'clicked' : null}
-      onClick={this.handleClick}>
-      Hello {this.props.name} {this.state.counter}
-      </div>
-    );
-  }
-}
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="Piwie" />, root);
+  ReactDOM.render(
+    <App />,
+    root
+  );
 }
