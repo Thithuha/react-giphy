4e539f383f45daec0e17d3656fca8c72c8912c75
thithuha nguyen <chloe.nguyen1910@gmail.com>
2021-10-06 18:09:41 +0200

    create GifList component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar';
 import Gif from './gif';
+import GifList from './gif_list';
 
 class App extends Component {
   render() {
+    const gifs = [
+      { id: "l3vRaLSB7dP96NTWw" },
+      { id: "5yLgoctdubLO8XGodOM" }
+    ];
+
     return (
       <div>
         <div className="left-scene">
           <SearchBar />
           <div className="selected-gif">
-          <Gif id="26FL4fdR9oRs2tdEA" />
+            <Gif id="l3vRaLSB7dP96NTWw" />
           </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+            <GifList gifs={gifs} />
+        </div>
       </div>
     );
   }
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Gif extends Component {
   render() {
-    const src = `https://media4.giphy.com/media/${this.props.id}/giphy.gif?cid=ecf05e47b87g5028bzolfpnl7355rdc1q9krifgxp7xbbr66&rid=giphy.gif&ct=g`;
+    const src = `https://media2.giphy.com/media/${this.props.id}/200.gif`;
     return (
       <img src={src} alt="" className="gif"/>
     );
